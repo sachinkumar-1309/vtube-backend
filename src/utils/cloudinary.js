8875ee7eff2a306c8fd0b931f0329d6fb8359814
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-04-22 15:02:53 +0530

    minute changes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -22,7 +22,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     );
     // file uploaded successfully
     // console.log("file is uploaded on cloudinary", response.url);
-    fs.unlinkSync(localFilePath); //delete the local file
+    fs.unlinkSync(localFilePath); //delete the local file after successfully being uploaded on cloudinary
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the locally saved temprorary files as the upload operation failed
@@ -32,37 +32,3 @@ const uploadOnCloudinary = async (localFilePath) => {
 };
 
 export { uploadOnCloudinary };
-// import { v2 as cloudinary } from "cloudinary";
-// import fs from "fs"; // File system
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// const uploadOnCloudinary = async (localFilePath) => {
-//   try {
-//     if (!localFilePath) return null;
-//     console.log("localFilePath: "+localFilePath)
-//     const response = await cloudinary.uploader.upload(localFilePath, {
-//       resource_type: "auto",
-//     });
-
-//     // File uploaded successfully
-//     console.log("File is uploaded on Cloudinary", response.url);
-//     return response;
-//   } catch (error) {
-//     // Check if the file exists before attempting to delete it
-//     if (fs.existsSync(localFilePath)) {
-//       // Remove the locally saved temporary files as the upload operation failed
-//       fs.unlinkSync(localFilePath);
-//       console.log("Local temporary file removed:", localFilePath);
-//     } else {
-//       console.log("File does not exist:", localFilePath);
-//     }
-//     return null;
-//   }
-// };
-
-// export { uploadOnCloudinary };
