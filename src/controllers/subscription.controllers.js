3711954e1236a8f0e836bfb2548edf98d80207d5
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-05-14 06:50:50 +0530

    Created subscription controller

---
+++ src/controllers/subscription.controllers.js
@@ -0,0 +1,32 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {User} from "../models/user.models.js"
+import { Subscription } from "../models/subscription.models.js"
+import {ApiError} from "../utils/ApiErrors.js"
+import {ApiResponse} from "../utils/ApiResponses.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    // TODO: toggle subscription
+    const {channelId} = req.params
+    if(!channelId){
+        throw new ApiError(404, "Channel ID is required")
+    }
+    
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
