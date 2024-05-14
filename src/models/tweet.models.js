14b3c8da3f7f6546dbcf17264ad6aa1f67f3e04c
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-05-14 06:48:45 +0530

    created tweet model

--- src/models/tweet.models.js
+++ src/models/tweet.models.js
@@ -3,7 +3,7 @@ import mongoose,{Schema} from "mongoose";
 const tweetSchema=new Schema({
     content:{
         type:String,
-        required:trusted,
+        required:true,
     },
     owner:{
         type:Schema.Types.ObjectId,
