7370fefbb7b4fed1b7a0dc232469711af7be2f12
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-05-14 06:50:29 +0530

    Eliminated error function

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,15 +16,13 @@ const uploadOnCloudinary = async (localFilePath) => {
       {
         resource_type: "auto",
       }, // TO CLOUDINARY:-Apne pata kro ki extention hai iska video hai ya image hai
-      function (error, result) {//Yhn pe error tha .... Yeh function accecpt krana tha
-        console.log(result);
-      }
     );
     // file uploaded successfully
     // console.log("file is uploaded on cloudinary", response.url);
     fs.unlinkSync(localFilePath); //delete the local file after successfully being uploaded on cloudinary
     return response;
   } catch (error) {
+    console.log("CLoudinary error: "+JSON.stringify(error))
     fs.unlinkSync(localFilePath); //remove the locally saved temprorary files as the upload operation failed
 
     return null;
